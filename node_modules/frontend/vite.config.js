293845ea7664c7a86d4d0227c9a3fae2a306c765
Richard Akrong <97akrichard@gmail.com>
2025-06-03 07:25:23 +0000

    fix

--- node_modules/frontend/vite.config.js
+++ node_modules/frontend/vite.config.js
@@ -1,46 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { visualizer as bundleAnalyzer } from "rollup-plugin-visualizer";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    include: ["react-icons", "react-router-dom", "framer-motion"],
-    
-  },
-  plugins: [
-    react({
-      jsxRuntime: "automatic",
-    }),
-    bundleAnalyzer({
-      analyzerMode: "static",
-      openAnalyzer: false,
-    }),
-  ],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-      "/uploads": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          icons: ["react-icons"],
-          vendor: ["react-router-dom", "framer-motion"],
-        },
-      },
-    },
-    minify: "esbuild",
-    target: "esnext",
-    chunkSizeWarningLimit: 1000,
-    cssCodeSplit: true,
-  },
-});
+  plugins: [react()],
+})
